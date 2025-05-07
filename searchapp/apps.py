1e57b3794a2013f913f532c3a0f2644ed5b9a981
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2025-05-06 18:45:18 -0700

    update searchapps/apps.py

--- searchapp/apps.py
+++ searchapp/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+import os
 
 class SearchappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "searchapp"
+    path = os.path.dirname(os.path.abspath(__file__))
